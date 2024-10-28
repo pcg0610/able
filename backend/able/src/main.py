d362199b3e890d79cb4723e840f4d7e48d2fc84d
parkrootseok <parkrootseok@gmail.com>
2024-10-28 17:38:04 +0900

    chore: init (#1)

--- backend/able/src/main.py
+++ backend/able/src/main.py
@@ -1,6 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
 
 @app.get("/")
 async def root():
