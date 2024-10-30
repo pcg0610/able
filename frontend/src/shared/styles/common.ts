3d92e2f6f78c4f68df275a67911afbb52e23b380
allkong <allempti@gmail.com>
2024-10-30 14:29:12 +0900

    design: Common 색상 변경

--- frontend/src/shared/styles/common.ts
+++ frontend/src/shared/styles/common.ts
@@ -1,20 +1,17 @@
 export const Common = {
   colors: {
+    background: '#F8F8F8',
+    black: '#1D1D20',
+    white: '#FFFFFF',
+
     primary: '#0051FF',
-    secondary: '#A2C5F9',
-    secondaryButton: '#D8E6FB',
+    secondary: '#DAE8FD',
     graphDetail: '#0D356F',
 
-    black: '#000000',
     gray500: '#3E3E3E',
     gray400: '#616161',
     gray300: '#B9B9B9',
     gray200: '#D7D7D7',
     gray100: '#F0F1F4',
-
-    background: '#F8F8F8',
-    white: '#FFFFFF',
   },
 };
-
-export default Common;
