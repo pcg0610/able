69c2d75638d6851fde2f3649814fde0fe9347e49
allkong <allempti@gmail.com>
2024-10-30 13:40:29 +0900

    feat: ArrowButton 구현

--- frontend/src/shared/styles/common.ts
+++ frontend/src/shared/styles/common.ts
@@ -1,18 +1,20 @@
 export const Common = {
   colors: {
-    primary: "#0051FF",
-    secondary: "#A2C5F9",
-    secondaryButton: "#D8E6FB",
-    graphDetail: "#0D356F",
+    primary: '#0051FF',
+    secondary: '#A2C5F9',
+    secondaryButton: '#D8E6FB',
+    graphDetail: '#0D356F',
 
-    black: "#000000",
-    gray500: "#3E3E3E",
-    gray400: "#616161",
-    gray300: "#B9B9B9",
-    gray200: "#D7D7D7",
-    gray100: "#F0F1F4",
+    black: '#000000',
+    gray500: '#3E3E3E',
+    gray400: '#616161',
+    gray300: '#B9B9B9',
+    gray200: '#D7D7D7',
+    gray100: '#F0F1F4',
 
-    background: "#F8F8F8",
-    white: "#FFFFFF",
+    background: '#F8F8F8',
+    white: '#FFFFFF',
   },
 };
+
+export default Common;
