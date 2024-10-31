6987dec7f651f6f5cfab96260bfb1ad4ea6797aa
allkong <allempti@gmail.com>
2024-10-31 18:50:05 +0900

    feat: DndProvider 설정

--- frontend/src/pages/canvas/canvas.page.tsx
+++ frontend/src/pages/canvas/canvas.page.tsx
@@ -1,3 +1,6 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import * as S from '@pages/canvas/canvas.style';
 
 import CanvasSidebar from '@features/canvas/ui/sidebar/canvas-sidebar';
@@ -6,13 +9,15 @@ import PageHeader from '@widgets/header/page-header';
 
 const CanvasPage = () => {
   return (
-    <S.PageContainer>
-      <PageHeader title='프로젝트' date='2024.08.12' />
-      <S.Content>
-        <CanvasSidebar />
-        <CanvasEditor />
-      </S.Content>
-    </S.PageContainer>
+    <DndProvider backend={HTML5Backend}>
+      <S.PageContainer>
+        <PageHeader title='프로젝트' date='2024.08.12' />
+        <S.Content>
+          <CanvasSidebar />
+          <CanvasEditor />
+        </S.Content>
+      </S.PageContainer>
+    </DndProvider>
   );
 };
 
