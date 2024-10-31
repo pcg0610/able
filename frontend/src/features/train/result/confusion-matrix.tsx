0c80f4f71082d55af5585c4ca09d223df6c163d8
조민주 <pida6716@gmail.com>
2024-10-31 16:44:18 +0900

    remove: 채우기용 코드 및 미사용 스타일 코드 삭제 (#9)

--- frontend/src/features/train/result/confusion-matrix.tsx
+++ frontend/src/features/train/result/confusion-matrix.tsx
@@ -1,21 +1,7 @@
-import React from 'react';
-import * as S from '@features/train/result/result.style';
 
-const ConfusionMatrix: React.FC = () => {
+const ConfusionMatrix = () => {
    return (
-      <S.GraphContainer>
-         <S.GraphTitle>Training and validation loss</S.GraphTitle>
-         <S.LegendContainer>
-            <S.LegendItem>
-               <S.BlueDot /> Smoothed training loss
-            </S.LegendItem>
-            <S.LegendItem>
-               <S.BlueLine /> Smoothed validation loss
-            </S.LegendItem>
-         </S.LegendContainer>
-         {/* 임시 그래프 공간 */}
-         <S.GraphPlaceholder>그래프가 여기에 표시됩니다.</S.GraphPlaceholder>
-      </S.GraphContainer>
+      <></>
    );
 };
 
