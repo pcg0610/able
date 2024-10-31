1f1bff8bad86e86c2db1d736301b90c4a72cce6b
조민주 <pida6716@gmail.com>
2024-10-31 16:34:46 +0900

    feat: 그래프 및 그래프 별 임시 데이터 추가 (#9)

--- frontend/src/features/train/result/result.style.tsx
+++ frontend/src/features/train/result/result.style.tsx
@@ -1,57 +1,64 @@
 import styled from '@emotion/styled';
+import Common from '@shared/styles/common';
 
-export const GraphContainer = styled.div`
+export const Container = styled.div`
+  padding: 2rem 6.25rem 2.87rem;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: ${Common.colors.background};
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  margin-bottom: 1.25rem;
+  flex-shrink: 0;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  gap: 1.25rem;
+  grid-template-rows: 1.1fr 1fr;
+  flex-grow: 1;
+  height: 100%;
+
+  & > .top-row {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1.6rem;
+  }
+
+  & > .bottom-row {
+    display: grid;
+    grid-template-columns: 1.1fr 0.7fr 1.2fr;
+    gap: 1.6rem;
+  }
+`;
+
+export const GraphCard = styled.div`
+  background: ${Common.colors.white};
   padding: 1.25rem;
   border-radius: .5rem;
-  background: #f9f9f9;
-  box-shadow: 0 .125rem .25rem rgba(0, 0, 0, 0.1);
+  box-shadow: 0 .0625rem .25rem rgba(0, 0, 0, 0.1);
+  border: .0625rem solid ${Common.colors.gray200};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const GraphTitle = styled.h3`
-  font-size: 1.2px;
-  font-weight: 500;
+  margin: .2rem 0 .525rem .625rem;
+  margin-right: auto;
+  font-size: ${Common.fontSizes.xl};
+  font-weight: ${Common.fontWeights.medium};
   color: #333;
 `;
 
-export const LegendContainer = styled.div`
-  display: flex;
-  gap: .625rem;
-  margin-top: .625rem;
-  margin-bottom: 1.25rem;
-`;
-
-export const LegendItem = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 0.9rem;
-  color: #666;
-`;
-
-export const BlueDot = styled.span`
-  width: .625rem;
-  height: .625rem;
-  background-color: #1f77b4;
-  border-radius: 50%;
-  display: inline-block;
-  margin-right: .3125rem;
-`;
-
-export const BlueLine = styled.span`
-  width: 1.25rem;
-  height: .125rem;
-  background-color: #1f77b4;
-  display: inline-block;
-  margin-right: .3125rem;
-`;
-
-export const GraphPlaceholder = styled.div`
-  height: 12.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 16px;
-  color: #999;
-  border: .0625rem dashed #ddd;
-  border-radius: .5rem;
-  margin-top: 1.25rem;
+export const F1ScoreTitle = styled(GraphTitle)`
+  font-weight: ${Common.fontWeights.semiBold}; 
+  margin-left: auto;
+  font-size: ${Common.fontSizes['3xl']};
 `;
