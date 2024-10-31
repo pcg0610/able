a5e5de6934c55abb5e004a7ff50a61d77f8ee7a3
박근석 <parkrootseok@gmail.com>
2024-10-31 16:16:46 +0900

    refactor: arg (#16)

--- backend/able/src/block/router.py
+++ backend/able/src/block/router.py
@@ -1,22 +1,34 @@
+import src.block.service as service
 from fastapi import APIRouter
 from src.block.enums import BlockType
 from src.block.schemas import BlocksResponse, BlockResponse
-from src.block.service import find_blocks_by_type
 from src.response.schemas import ResponseModel
 from src.response.utils import ok
 
 block_router = router = APIRouter()
 
-@router.get("", response_model=ResponseModel[BlocksResponse])
-def blocks_by_type(type: BlockType):
+@router.get(
+    path="",
+    response_model=ResponseModel[BlocksResponse],
+    summary="타입별 블록 조회",
+    description="해당 타입을 가진 블록 목록을 조회한다."
+)
+def find_blocks_by_type(type: BlockType):
     return ok(
         data=BlocksResponse(
-            blocks=find_blocks_by_type(type)
+            blocks=service.find_blocks_by_type(type)
         )
     )
 
-@router.get("/search", response_model=BlockResponse)
+@router.get(
+    path="/search",
+    response_model=BlockResponse,
+    summary="블록 검색",
+    description="키워드를 가지는 블록에 대하여 검색한다."
+)
 def search(keyword: str):
     return ok(
-        data=BlockResponse(blocks=search(keyword))
+        data=BlockResponse(
+            blocks=service.search(keyword)
+        )
     )
