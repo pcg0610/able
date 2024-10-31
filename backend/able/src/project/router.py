19d57cbcab8a5789542b5e1b9ef6e11579fc5aac
박다솔 <padasol@naver.com>
2024-10-31 13:32:32 +0900

    docs: 프로젝트 API Swagger 설명 작성 (#19)

--- backend/able/src/project/router.py
+++ backend/able/src/project/router.py
@@ -8,27 +8,32 @@ from src.response.utils import created, ok, no_content
 
 project_router = router = APIRouter()
 
-@router.post("", response_model=Response)
+@router.post("", response_model=Response,
+             summary="프로젝트 생성", description="")
 async def create_project(project: Project):
     service.create_project(project)
     return created()
 
-@router.get("/{title}", response_model=Response)
+@router.get("/{title}", response_model=Response,
+            summary="프로젝트 단일 조회", description="프로젝트 이름으로 조회")
 async def get_project(title: str):
     project = service.get_project(title)
     return ok(project)
 
-@router.get("/", response_model=Response)
+@router.get("/", response_model=Response,
+            summary="프로젝트 목록 조회", description="")
 async def get_projects():
     projects = service.get_projects()
     return ok(projects)
 
-@router.put("", response_model=Response)
+@router.put("", response_model=Response,
+            summary="프로젝트 정보 수정", description="변경 전 프로젝트 이름, 설명 포함 필요")
 async def update_project(project: UpdatedProject):
     service.update_project(project)
     return ok(True)
 
-@router.delete("/{title}", response_model=Response)
+@router.delete("/{title}", response_model=Response,
+               summary="프로젝트 삭제", description="프로젝트 이름으로 삭제")
 async def delete_project(title: str):
     service.delete_project(title)
     return no_content()
