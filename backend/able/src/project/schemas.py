dfcfdb1e9dce416208cfbb61d0768f0b4ea8f03b
박다솔 <padasol@naver.com>
2024-10-31 15:11:17 +0900

    fix: 단일 프로젝트 조회 오류 해결 (#8)

--- backend/able/src/project/schemas.py
+++ backend/able/src/project/schemas.py
@@ -1,4 +1,5 @@
 from src.response.schemas import ImmutableBaseModel
+from pydantic import BaseModel
 
 class Project(ImmutableBaseModel):
     title: str                          # 프로젝트명 
@@ -6,8 +7,10 @@ class Project(ImmutableBaseModel):
     cuda_version: str                   # 쿠다 버전
     python_kernel_path: str             # 파이썬 커널
 
-class SelectedProject(ImmutableBaseModel):
+class SelectedProject(Project):
     thumbnail: str | None = None        # 썸네일
+    class Config:
+        frozen = False  # 불변성 해제
 
 class UpdatedProject(ImmutableBaseModel):
     prev_title: str                     # 변경 전 프로젝트명
