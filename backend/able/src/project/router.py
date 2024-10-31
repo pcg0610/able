be02a53adc60b7c59c4e264120185d638cabb414
parkrootseok <parkrootseok@gmail.com>
2024-10-31 15:20:40 +0900

    refact: 공통 응답 (#17)
    - 제네릭 타입을 사용하도록 변경

--- backend/able/src/project/router.py
+++ backend/able/src/project/router.py
@@ -3,36 +3,36 @@ import src.project.service as service
 from fastapi import APIRouter, status
 
 from src.project.schemas import Project, UpdatedProject
-from src.response.schemas import Response
+from src.response.schemas import ResponseModel
 from src.response.utils import created, ok, no_content
 
 project_router = router = APIRouter()
 
-@router.post("", response_model=Response,
+@router.post("", response_model=ResponseModel,
              summary="프로젝트 생성", description="")
 async def create_project(project: Project):
     service.create_project(project)
     return created()
 
-@router.get("/{title}", response_model=Response,
+@router.get("/{title}", response_model=ResponseModel[Project],
             summary="프로젝트 단일 조회", description="프로젝트 이름으로 조회")
 async def get_project(title: str):
     project = service.get_project(title)
-    return ok(project)
+    return ok(data=project)
 
-@router.get("/", response_model=Response,
+@router.get("/", response_model=ResponseModel[Project],
             summary="프로젝트 목록 조회", description="")
 async def get_projects():
     projects = service.get_projects()
-    return ok(projects)
+    return ok(data=projects)
 
-@router.put("", response_model=Response,
+@router.put("", response_model=ResponseModel[bool],
             summary="프로젝트 정보 수정", description="변경 전 프로젝트 이름, 설명 포함 필요")
 async def update_project(project: UpdatedProject):
     service.update_project(project)
     return ok(True)
 
-@router.delete("/{title}", response_model=Response,
+@router.delete("/{title}", response_model=ResponseModel,
                summary="프로젝트 삭제", description="프로젝트 이름으로 삭제")
 async def delete_project(title: str):
     service.delete_project(title)
--- backend/able/src/response/utils.py
+++ backend/able/src/response/utils.py
@@ -1,22 +1,23 @@
 import logging
-from typing import Any
+from typing import TypeVar
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
-from src.response.schemas import Response
+from src.response.schemas import ResponseModel
 
 logger = logging.getLogger(__name__)
+T = TypeVar("T")
 
-def ok(data: Any) -> Response:
-    return Response(
+def ok(data: T) -> ResponseModel[T]:
+    return ResponseModel[T](
             status_code=HTTP_200_OK,
             data=data
         )
 
-def created() -> Response:
-    return Response(
+def created() -> ResponseModel:
+    return ResponseModel(
         status_code=HTTP_201_CREATED
     )
 
-def no_content() -> Response:
-    return Response(
+def no_content() -> ResponseModel:
+    return ResponseModel(
         status_code=HTTP_204_NO_CONTENT
     )
