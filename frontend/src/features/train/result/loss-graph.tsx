96327ee084f96eccb8b3bc6ab25bed3c83f4a83d
조민주 <pida6716@gmail.com>
2024-10-31 16:35:25 +0900

    style: loss-graph 스타일 수정 및 데이터 형식 추가 (#9)

--- frontend/src/features/train/result/loss-graph.tsx
+++ frontend/src/features/train/result/loss-graph.tsx
@@ -12,25 +12,14 @@ import {
 
 import Common from '@shared/styles/common';
 
-const data = [
-   { epoch: 0, trainingLoss: 0.02, validationLoss: 0.12 },
-   { epoch: 10, trainingLoss: 0.07, validationLoss: 0.11 },
-   { epoch: 20, trainingLoss: 0.05, validationLoss: 0.14 },
-   { epoch: 30, trainingLoss: 0.13, validationLoss: 0.15 },
-   { epoch: 40, trainingLoss: 0.10, validationLoss: 0.16 },
-   { epoch: 50, trainingLoss: 0.18, validationLoss: 0.14 },
-   { epoch: 60, trainingLoss: 0.20, validationLoss: 0.18 },
-   { epoch: 70, trainingLoss: 0.26, validationLoss: 0.19 },
-   { epoch: 80, trainingLoss: 0.28, validationLoss: 0.17 },
-];
+const LossGraph = ({ data }) => {
 
-const LossGraph = () => {
    return (
       <ResponsiveContainer width="100%" height="100%">
          <ComposedChart data={data} margin={{ top: 10, right: 30, left: 10, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" stroke={Common.colors.gray200} />
-            <XAxis dataKey="epoch" />
-            <YAxis domain={[0, 0.3]} />
+            <XAxis dataKey="epoch" tick={{ fontSize: Common.fontSizes.xs }} />
+            <YAxis domain={[0, 0.3]} tick={{ fontSize: Common.fontSizes.xs }} />
             <Tooltip />
             <Legend
                verticalAlign="top"
@@ -39,7 +28,7 @@ const LossGraph = () => {
                height={40}
                iconSize={10}
                formatter={(value) => (
-                  <span style={{ color: Common.colors.graphDetail }}>{value}</span>
+                  <span style={{ color: Common.colors.graphDetail, fontSize: Common.fontSizes.xs }}>{value}</span>
                )}
                payload={[
                   { value: 'Smoothed training loss', type: 'circle', color: Common.colors.primary },
@@ -48,7 +37,7 @@ const LossGraph = () => {
             />
             <Area
                type="monotone"
-               dataKey="validationLoss"
+               dataKey="validation"
                stroke={Common.colors.primary}
                fill={Common.colors.secondary}
                strokeWidth={2}
@@ -57,9 +46,9 @@ const LossGraph = () => {
             />
             <Line
                type="monotone"
-               dataKey="trainingLoss"
+               dataKey="training"
                stroke={Common.colors.primary}
-               strokeWidth={6}
+               strokeWidth={5}
                strokeDasharray="8 4"
                dot={false}
                name="Smoothed training loss"
