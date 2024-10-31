9b48e27f358b0a75f1930e9000b5f3b4bfb50e05
조민주 <pida6716@gmail.com>
2024-10-31 17:08:04 +0900

    feat: epoch-graph 및 색상 추가 (#9)

--- frontend/src/shared/styles/common.ts
+++ frontend/src/shared/styles/common.ts
@@ -1,18 +1,20 @@
 const Common = {
   colors: {
-    background: '#F8F8F8',
-    black: '#1D1D20',
-    white: '#FFFFFF',
+    background: "#F8F8F8",
+    black: "#1D1D20",
+    white: "#FFFFFF",
 
-    primary: '#0051FF',
-    secondary: '#DAE8FD',
-    graphDetail: '#0D356F',
+    primary: "#0051FF",
+    secondary: "#DAE8FD",
+    graphDetail: "#0D356F",
 
-    gray500: '#3E3E3E',
-    gray400: '#616161',
-    gray300: '#B9B9B9',
-    gray200: '#D7D7D7',
-    gray100: '#F0F1F4',
+    accuracyGraph: "#FF7A41",
+
+    gray500: "#3E3E3E",
+    gray400: "#616161",
+    gray300: "#B9B9B9",
+    gray200: "#D7D7D7",
+    gray100: "#F0F1F4",
   },
   fontWeights: {
     regular: 400,
@@ -20,15 +22,15 @@ const Common = {
     semiBold: 600,
   },
   fontSizes: {
-    '2xs': '0.625rem', // 10px
-    xs: '.75rem', // 12px
-    sm: '.875rem', // 14px
-    base: '1rem', // 16px
-    lg: '1.125rem', // 18px
-    xl: '1.25rem', // 20px
-    '2xl': '1.5rem', // 24px
-    '3xl': '1.75rem', // 28px
-    '4xl': '2.25rem', // 36px
+    "2xs": "0.625rem", // 10px
+    xs: ".75rem", // 12px
+    sm: ".875rem", // 14px
+    base: "1rem", // 16px
+    lg: "1.125rem", // 18px
+    xl: "1.25rem", // 20px
+    "2xl": "1.5rem", // 24px
+    "3xl": "1.75rem", // 28px
+    "4xl": "2.25rem", // 36px
   },
 };
 
