0eaacca8b31be6b0e545b4734f9bc8a6fb2a74fb
allkong <allempti@gmail.com>
2024-11-01 10:46:43 +0900

    feat: 캔버스 페이지에 ReactFlowProvider 추가

--- frontend/src/pages/canvas/canvas.page.tsx
+++ frontend/src/pages/canvas/canvas.page.tsx
@@ -1,8 +1,9 @@
+// src/pages/canvas/canvas-page.tsx
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { ReactFlowProvider } from '@xyflow/react';
 
 import * as S from '@pages/canvas/canvas.style';
-
 import CanvasSidebar from '@features/canvas/ui/sidebar/canvas-sidebar';
 import CanvasEditor from '@features/canvas/ui/canvas-editor';
 import PageHeader from '@widgets/header/page-header';
@@ -10,13 +11,15 @@ import PageHeader from '@widgets/header/page-header';
 const CanvasPage = () => {
   return (
     <DndProvider backend={HTML5Backend}>
-      <S.PageContainer>
-        <PageHeader title='프로젝트' date='2024.08.12' />
-        <S.Content>
-          <CanvasSidebar />
-          <CanvasEditor />
-        </S.Content>
-      </S.PageContainer>
+      <ReactFlowProvider>
+        <S.PageContainer>
+          <PageHeader title='프로젝트' date='2024.08.12' />
+          <S.Content>
+            <CanvasSidebar />
+            <CanvasEditor />
+          </S.Content>
+        </S.PageContainer>
+      </ReactFlowProvider>
     </DndProvider>
   );
 };
