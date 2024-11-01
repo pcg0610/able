564772a4d143d40078e03c760623d4a376fb4d99
조민주 <pida6716@gmail.com>
2024-11-01 17:23:07 +0900

    design: 에포크 사이드 바 클릭 애니메이션 추가 (#23)

--- frontend/src/features/train/ui/sidebar/epoch-list-sidebar.style.tsx
+++ frontend/src/features/train/ui/sidebar/epoch-list-sidebar.style.tsx
@@ -6,20 +6,25 @@ export const SidebarContainer = styled.div`
   height: 100%;
   padding: 1rem;
   background-color: ${Common.colors.white};
-  box-shadow: .125rem 0 .3125rem rgba(73, 73, 73, 0.1);
-  border-right: .0625rem solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0.125rem 0 0.3125rem rgba(73, 73, 73, 0.1);
+  border-right: 0.0625rem solid rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
 `;
 
-export const EpochItem = styled.div`
+export const EpochItem = styled.div<{ isSelected: boolean }>`
   padding: 0.75rem;
-  background-color: ${Common.colors.gray100};
+  background-color: ${(props) =>
+    props.isSelected ? Common.colors.gray200 : "transparent"};
   border-radius: 0.375rem;
   font-weight: ${Common.fontWeights.regular};
   font-size: 0.9375rem;
   cursor: pointer;
+  transition-property: background-color;
+  transition-duration: 0.6s;
+
+
   &:hover {
     background-color: ${Common.colors.gray200};
   }
--- frontend/src/features/train/ui/sidebar/epoch-list-sidebar.tsx
+++ frontend/src/features/train/ui/sidebar/epoch-list-sidebar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { SidebarContainer, EpochItem } from "@features/train/ui/sidebar/epoch-list-sidebar.style";
 
 const EpochListSidebar = () => {
+   const [selectedEpoch, setSelectedEpoch] = useState("20241222_epoch_10.pth");
+
    const epochs = [
       "20241222_epoch_10.pth",
       "20241222_epoch_20.pth",
@@ -8,10 +11,20 @@ const EpochListSidebar = () => {
       "20241222_epoch_40.pth",
    ];
 
+   const handleClick = (epoch: string) => {
+      setSelectedEpoch(epoch);
+   };
+
    return (
       <SidebarContainer>
          {epochs.map((epoch, index) => (
-            <EpochItem key={index}>{epoch}</EpochItem>
+            <EpochItem
+               key={index}
+               isSelected={selectedEpoch === epoch}
+               onClick={() => handleClick(epoch)}
+            >
+               {epoch}
+            </EpochItem>
          ))}
       </SidebarContainer>
    );
