f4414f67ba384d808d8020989b2aa4526b069c0a
조민주 <pida6716@gmail.com>
2024-11-01 16:22:14 +0900

    design : 버튼에 너비 높이 설정 추가 및 스타일 수정 (#26)

--- frontend/src/shared/ui/button/basic-button.style.tsx
+++ frontend/src/shared/ui/button/basic-button.style.tsx
@@ -2,14 +2,20 @@ import styled from '@emotion/styled';
 
 import Common from '@shared/styles/common';
 
-export const StyledButton = styled.button<{ color: string; backgroundColor: string }>`
+export const StyledButton = styled.button<{
+  color: string; backgroundColor: string;
+  width?: string; height?: string;
+}>`
   background-color: ${(props) => props.backgroundColor};
   color: ${(props) => props.color};
   border: none;
   border-radius: .375rem;
-  padding: 0.75rem 1.875rem; 
   font-size: ${Common.fontSizes.base};
   font-weight: ${Common.fontWeights.semiBold};
+  padding: ${(props) => (props.height ? '0' : '0.75rem 1.875rem')};
+  width: ${(props) => props.width || 'auto'};
+  height: ${(props) => props.height || 'auto'};
+  justify-content: center;
   cursor: pointer;
   display: flex;  
   align-items: center;  
