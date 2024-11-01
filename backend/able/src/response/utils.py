3edc7f4deefedd2722bf82054424b5eff48b6c10
parkrootseok <parkrootseok@gmail.com>
2024-11-01 16:47:49 +0900

    refactor: http 상태 코드 반영 (#17)

--- backend/able/src/response/utils.py
+++ backend/able/src/response/utils.py
@@ -1,4 +1,5 @@
 import logging
+from fastapi import Response
 from typing import TypeVar
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from src.response.schemas import ResponseModel
@@ -6,18 +7,30 @@ from src.response.schemas import ResponseModel
 logger = logging.getLogger(__name__)
 T = TypeVar("T")
 
-def ok(data: T) -> ResponseModel[T]:
-    return ResponseModel[T](
-            status_code=HTTP_200_OK,
-            data=data
-        )
-
-def created() -> ResponseModel:
-    return ResponseModel(
-        status_code=HTTP_201_CREATED
+def ok(data: T) -> Response:
+    status_code=HTTP_200_OK
+    content = ResponseModel[T](
+        status_code=status_code,
+        data=data
     )
 
-def no_content() -> ResponseModel:
-    return ResponseModel(
-        status_code=HTTP_204_NO_CONTENT
+    return Response(
+        content=content.model_dump_json(exclude_none=True),
+        status_code=status_code,
+        media_type="application/json",
+    )
+
+def created() -> Response:
+    status_code=HTTP_201_CREATED
+
+    return Response(
+        status_code = status_code,
+        media_type="application/json"
+    )
+
+def no_content() -> Response:
+    status_code = HTTP_204_NO_CONTENT
+    return Response(
+        status_code=status_code,
+        media_type="application/json"
     )
