e16c522e4e255d1ce95426883a5ecf81a5043758
조민주 <pida6716@gmail.com>
2024-11-01 14:02:08 +0900

    feat: home page header 추가 (#26)

---
+++ frontend/src/features/home/ui/header/home-header.tsx
@@ -0,0 +1,13 @@
+import * as S from '@features/home/ui/header/home-header.style';
+
+const HomeHeader = () => {
+
+   return (
+      <S.Header>
+         <S.LeftText>ABLE</S.LeftText>
+         <S.RightText>docs</S.RightText>
+      </S.Header>
+   );
+};
+
+export default HomeHeader;
