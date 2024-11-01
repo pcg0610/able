215e95bcadd14af43575ecf985e0876833f51ed9
박찬규 <cham9906@naver.com>
2024-11-01 16:38:01 +0900

    feat: 학습 블록 필터링 기능 구현 (#25)

--- backend/able/src/train/service.py
+++ backend/able/src/train/service.py
@@ -1,6 +1,6 @@
 from .utils import Trainer, validate_file_format, create_dataset, create_data_loader, convert_block_graph_to_model, \
     convert_criterion_block_to_module, TrainLogger, convert_optimizer_block_to_optimizer, split_dataset, \
-    create_data_preprocessor
+    create_data_preprocessor, filter_blocks_connected_to_data
 from . import TrainRequestDto
 
 def train(train_request_dto: TrainRequestDto):
@@ -9,18 +9,24 @@ def train(train_request_dto: TrainRequestDto):
     if not validate_file_format(data_path, "json"):
         raise Exception()
 
-    transforms = create_data_preprocessor(train_request_dto.transforms)
+    transform_blocks, loss_blocks, optimizer_blocks, other_blocks = filter_blocks_connected_to_data(
+        train_request_dto.data, train_request_dto.transforms, train_request_dto.loss, train_request_dto.optimizer,
+        train_request_dto.blocks, train_request_dto.edges)
+
+    #TODO: 학습할 모델 그래프 저장 기능 추가
+
+    transforms = create_data_preprocessor(transform_blocks)
 
     dataset = create_dataset(data_path, transforms)
 
-    model = convert_block_graph_to_model(train_request_dto.blocks, train_request_dto.edges)
+    model = convert_block_graph_to_model(other_blocks, train_request_dto.edges)
 
     if model is None:
         raise Exception()
 
-    criterion = convert_criterion_block_to_module(train_request_dto.loss)
+    criterion = convert_criterion_block_to_module(loss_blocks)
 
-    optimizer = convert_optimizer_block_to_optimizer(train_request_dto.optimizer, model.parameters())
+    optimizer = convert_optimizer_block_to_optimizer(optimizer_blocks, model.parameters())
 
     trainer = Trainer(model, dataset, criterion, optimizer, train_request_dto.batch_size, TrainLogger(train_request_dto.project_name))
 
