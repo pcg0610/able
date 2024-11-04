10a4bc204c3a2de855b3515c2187e47a0abb20bf
조민주 <pida6716@gmail.com>
2024-11-04 15:04:14 +0900

    feat: deploy 페이지 추가 (#35)

---
+++ frontend/src/pages/deploy/deploy.style.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  
+`;
+
+export const Content = styled.div`
+  flex: 1;
+`;
