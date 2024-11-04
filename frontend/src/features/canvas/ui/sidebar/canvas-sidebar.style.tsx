a3980d53c0c05d00e1347b1c52d91f7506e66fa0
allkong <allempti@gmail.com>
2024-11-04 14:01:24 +0900

    design: 사이드바 스크롤 숨기기

--- frontend/src/features/canvas/ui/sidebar/canvas-sidebar.style.tsx
+++ frontend/src/features/canvas/ui/sidebar/canvas-sidebar.style.tsx
@@ -12,4 +12,14 @@ export const SidebarContainer = styled.div`
 
   border-right: 1px solid ${Common.colors.gray200};
   padding: 0.9375rem;
+
+  /* 스크롤바 디자인 숨기기 */
+  &::-webkit-scrollbar {
+    width: 0; /* 웹킷 브라우저에서 스크롤바 너비를 0으로 설정하여 숨김 */
+    background: transparent;
+  }
+
+  /* Firefox의 경우 스크롤바 숨기기 */
+  scrollbar-width: none; /* Firefox에서 스크롤바 제거 */
+  -ms-overflow-style: none; /* IE와 Edge에서 스크롤바 제거 */
 `;
