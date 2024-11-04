f343f0fa3c4a944117962c327016f59857731567
allkong <allempti@gmail.com>
2024-11-04 14:54:56 +0900

    feat: 블록 응답 데이터 타입 지정

--- frontend/src/features/canvas/api/use-blocks.query.ts
+++ frontend/src/features/canvas/api/use-blocks.query.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import axiosInstance from '@shared/api/axios-instance';
 import canvasKey from '@features/canvas/api/canvas-key';
+import { BlocksResponse } from '../types/block-types.type';
 
 // get-fetch, post-create, put-update, delete-delete
 const fetchBlocks = async (type: string) => {
@@ -15,7 +16,7 @@ const fetchBlocks = async (type: string) => {
 };
 
 export const useBlocks = (type: string, isOpen: boolean) => {
-  return useQuery({
+  return useQuery<BlocksResponse>({
     queryKey: canvasKey.blocks(type),
     queryFn: () => fetchBlocks(type),
     enabled: isOpen,
