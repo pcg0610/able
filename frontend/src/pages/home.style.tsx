595ce7f14ac267f1ea1ee4347a3e092dcb0176eb
조민주 <pida6716@gmail.com>
2024-11-04 15:30:07 +0900

    design: 스크롤 스타일 변경 (#26)

--- frontend/src/pages/home.style.tsx
+++ frontend/src/pages/home.style.tsx
@@ -11,4 +11,15 @@ export const ContentContainer = styled.div`
   overflow-y: auto; 
   height: calc(100% - 2.5rem);
   box-sizing: border-box;
+
+  &::-webkit-scrollbar {
+    width: 0.9375rem;
+    height: 0.5rem;
+    border-radius: 0.375rem;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 0.375rem;
+  }
 `;
