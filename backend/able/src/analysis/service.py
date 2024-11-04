0e7866d26f398014fcc788b6b9516c0bb6cf4462
박다솔 <padasol@naver.com>
2024-11-04 17:01:03 +0900

    feat: 분석 실행 후 각 conv레이어에 대해 피쳐맵 생성 후 최종 히트맵 생성 기능 구현 (#34)

--- backend/able/src/analysis/service.py
+++ backend/able/src/analysis/service.py
@@ -2,10 +2,13 @@ import logging
 
 from pathlib import Path
 from typing import List
+from fastapi import UploadFile
 
 from src.file.path_manager import PathManager
-from src.file.utils import get_directory, read_image_file
+from src.file.utils import get_directory, read_image_file, save_img, create_directory
 from src.utils import encode_image_to_base64, get_epoch_id
+from src.analysis.utils import FeatureMapExtractor, read_blocks
+from src.train.utils import split_blocks
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,3 +28,33 @@ def get_result(project_name: str, result_name: str, epoch_name:str, block_id: st
     feature_map_image = encode_image_to_base64(read_image_file(feature_map_path / image_name))
 
     return feature_map_image
+
+async def analysis(project_name: str, result_name: str, epoch_name:str, file: UploadFile) -> str:
+
+    #block_graph.json 파일에서 블록, 엣지 정보 읽어오기
+    block_graph_path = pathManager.get_train_result_path(project_name, result_name) / "block_graph.json"
+    block_graph = read_blocks(block_graph_path)
+    blocks = block_graph.blocks
+    edges = block_graph.edges    
+
+    # 블록 카테고리 별로 나누기
+    _, transform_blocks, _, _, other_blocks = split_blocks(blocks)
+
+    epoch_path = pathManager.get_epoch_path(project_name, result_name, get_epoch_id(epoch_name))
+    feature_maps_path = pathManager.get_feature_maps_path(project_name, result_name, get_epoch_id(epoch_name))
+
+    # 피쳐맵을 저장할 디렉터리 생성
+    create_directory(feature_maps_path)
+
+    # 원본 이미지 저장
+    img_path = await save_img(feature_maps_path, "original.jpg", file)
+
+    extractor = FeatureMapExtractor(epoch_path, feature_maps_path, other_blocks, edges, device="cpu")
+    img_tensor = extractor.preprocess_image(img_path, transform_blocks=transform_blocks)
+    extractor.run_inference(img_tensor)
+    extractor.save_feature_maps(img_path)
+    extractor.save_heatmap(img_path)
+
+
+    heatmap_img = encode_image_to_base64(read_image_file(img_path))
+    return heatmap_img
