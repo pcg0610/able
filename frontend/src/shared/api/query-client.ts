0b0e9bc3c5a8c6adebdd051e6c0eb535f7f84c61
allkong <allempti@gmail.com>
2024-11-04 14:37:59 +0900

    feat: 캐싱 staleTime 설정

--- frontend/src/shared/api/query-client.ts
+++ frontend/src/shared/api/query-client.ts
@@ -3,6 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      staleTime: 1000 * 60 * 5, // 5분
       retry: 0,
       refetchOnWindowFocus: false,
     },
