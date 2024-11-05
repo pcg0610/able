0ae3273b2bb6a2d7ca7d30436a9734e637517958
allkong <allempti@gmail.com>
2024-11-05 16:35:06 +0900

    design: BasicButton 스타일 변경

--- frontend/src/shared/ui/button/basic-button.style.tsx
+++ frontend/src/shared/ui/button/basic-button.style.tsx
@@ -3,23 +3,29 @@ import styled from '@emotion/styled';
 import Common from '@shared/styles/common';
 
 export const StyledButton = styled.button<{
-  color: string; backgroundColor: string;
-  width?: string; height?: string;
+  color: string;
+  backgroundColor: string;
+  width?: string;
+  height?: string;
 }>`
   background-color: ${(props) => props.backgroundColor};
   color: ${(props) => props.color};
+
   border: none;
-  border-radius: .375rem;
+  border-radius: 0.375rem;
+
   font-size: ${Common.fontSizes.base};
   font-weight: ${Common.fontWeights.semiBold};
-  padding: ${(props) => (props.height ? '0' : '0.75rem 1.875rem')};
+
   width: ${(props) => props.width || 'auto'};
   height: ${(props) => props.height || 'auto'};
+  display: flex;
   justify-content: center;
+  align-items: center;
+
+  padding: ${(props) => (props.height ? '0' : '0.75rem 0')};
+  gap: 0.5rem;
   cursor: pointer;
-  display: flex;  
-  align-items: center;  
-  gap: 0.5rem; 
 
   transition: background-color 0.3s, filter 0.3s;
 
@@ -35,4 +41,4 @@ export const StyledButton = styled.button<{
 export const StyledIcon = styled.span`
   display: flex;
   align-items: center;
-`
+`;
