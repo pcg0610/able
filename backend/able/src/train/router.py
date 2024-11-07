10838bb26260d4034834cd7f5812e4dd407f700e
박상후 <tkdgn407@naver.com>
2024-11-07 11:13:43 +0900

    11/07 11:12 feature/46-train-status => develop

--- backend/able/src/train/router.py
+++ backend/able/src/train/router.py
@@ -2,27 +2,29 @@ from fastapi import APIRouter, BackgroundTasks
 from starlette.responses import Response
 
 from .dto import TrainResultRequest, TrainRequest
-from .service import train_in_background, load_train_result
+from .service import train as train_service, load_train_result
 from src.train.schemas import TrainResultResponse
-from src.response.utils import ok, accepted
+from src.response.utils import ok
+from ..response.schemas import ResponseModel
+from ..response.utils import accepted
 
 train_router = router = APIRouter()
 
 @router.get(
-    path="/result",
-    response_model=TrainResultResponse,
+    path="/result/{project_name}/{train_result_name}",
+    response_model=ResponseModel[TrainResultResponse],
     summary="학습 결과 조회",
     description="프로젝트 이름, 학습 결과 이름에 대하여 조회한다."
 )
-def get_train_result(request: TrainResultRequest):
+def get_train_result(project_name: str, train_result_name: str):
     return ok(
         data=TrainResultResponse(
-            train_result=load_train_result(request)
+            train_result=load_train_result(project_name, train_result_name)
         )
     )
 
 
 @router.post("")
 async def train(request: TrainRequest, background_tasks: BackgroundTasks) -> Response:
-    background_tasks.add_task(train_in_background, request)
+    background_tasks.add_task(train_service(request))
     return accepted()
