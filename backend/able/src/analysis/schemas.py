86539d60e5e683fafd75e84fd94aa916fd9f4f41
박다솔 <padasol@naver.com>
2024-11-07 11:11:55 +0900

    feat: 블록 아이디 리스트로 피쳐맵 리스트 반환 (#49)

--- backend/able/src/analysis/schemas.py
+++ backend/able/src/analysis/schemas.py
@@ -1,8 +1,21 @@
-from typing import List
+from typing import List, Optional
 from src.response.schemas import ImmutableBaseModel
+
+class FeatureMap(ImmutableBaseModel):
+    block_id: str
+    img: Optional[str]
 
 class EpochsResponse(ImmutableBaseModel):
     epochs : List[str]
 
 class ImageResponse(ImmutableBaseModel):
     image: str
+
+class FeatureMapRequest(ImmutableBaseModel):
+    project_name: str
+    result_name: str
+    epoch_name: str
+    block_id: List[str]
+
+class FeatureMapResponse(ImmutableBaseModel):
+    feature_map: List[FeatureMap]
