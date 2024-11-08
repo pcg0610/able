5a4a1d3c6566cee4574c3351a94b4dd693a36f55
조민주 <pida6716@gmail.com>
2024-11-08 10:53:12 +0900

    feat: add resultName, epochName (#57)

--- frontend/src/entities/project/types/project.type.ts
+++ frontend/src/entities/project/types/project.type.ts
@@ -2,7 +2,11 @@ import type { Project } from '@features/home/types/home.type';
 
 export interface ProjectNameState {
   projectName: string;
+  resultName: string;
+  epochName: string;
   setProjectName: (project: string) => void;
+  setResultName: (result: string) => void;
+  setEpochName: (epoch: string) => void;
 }
 
 export interface ProjectState {
