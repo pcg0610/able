2b9a73c8340299d03a9dc61474bf5c61b9d103e2
박다솔 <padasol@naver.com>
2024-11-11 15:38:55 +0900

    fix: 프로젝트 필수 필드를 선택 필드로 변경 (#69)

--- backend/able/src/project/schemas.py
+++ backend/able/src/project/schemas.py
@@ -4,8 +4,8 @@ from typing import List
 class Project(ImmutableBaseModel):
     title: str                          # 프로젝트명 
     description: str | None = None      # 프로젝트 설명(선택)
-    cuda_version: str                   # 쿠다 버전
-    python_kernel_path: str             # 파이썬 커널
+    cuda_version: str  | None = None
+    python_kernel_path: str | None = None
 
 class SelectedProject(Project):
     thumbnail: str | None = None        # 썸네일
