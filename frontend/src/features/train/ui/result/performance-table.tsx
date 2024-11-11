546dca127a67fc9277c96cb0ad2dc23c5a4ed8c3
조민주 <pida6716@gmail.com>
2024-11-11 17:43:45 +0900

    feat: performance table api 연동(#57)

--- frontend/src/features/train/ui/result/performance-table.tsx
+++ frontend/src/features/train/ui/result/performance-table.tsx
@@ -1,15 +1,19 @@
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
 import Common from '@shared/styles/common';
+import { PerformanceMatrics } from '@features/train/types/analyze.type';
 
-const data = [
-  { name: 'Accuracy', value: 30 },
-  { name: 'Top-5\nAccuracy', value: 70 },
-  { name: 'Precision', value: 50 },
-  { name: 'Recall', value: 90 },
-];
+interface PerformanceTableProps {
+  performanceMetrics?: PerformanceMatrics;
+}
 
-const PerformanceTable = () => {
+const PerformanceTable = ({ performanceMetrics = { accuracy: 0, top5Accuracy: 0, precision: 0, recall: 0 } }: PerformanceTableProps) => {
+  const data = [
+    { name: 'Accuracy', value: performanceMetrics.accuracy * 100 },
+    { name: 'Top-5\nAccuracy', value: performanceMetrics.top5Accuracy * 100 },
+    { name: 'Precision', value: performanceMetrics.precision * 100 },
+    { name: 'Recall', value: performanceMetrics.recall * 100 },
+  ];
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart data={data} layout="vertical" margin={{ top: 15, right: 25, left: 10, bottom: 0 }}>
