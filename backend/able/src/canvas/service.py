9099a42f7e9d227ea0d518598a681ab1c8a7ab86
박다솔 <padasol@naver.com>
2024-11-11 15:13:57 +0900

    feat: 캔버스 저장 시 썸네일 저장 기능 추가 (#65)

--- backend/able/src/canvas/service.py
+++ backend/able/src/canvas/service.py
@@ -1,12 +1,13 @@
 import logging
 from src.canvas.schemas import SaveCanvasRequest, Canvas
-from src.file.utils import get_file, create_file
+from src.file.utils import get_file, create_file, save_img_from_base64
 from src.file.path_manager import PathManager
 from src.utils import str_to_json, json_to_str
 
 logger = logging.getLogger(__name__)
 path_manager = PathManager()
 block_graph = "block_graph.json"
+thumbnail = "thumbnail.jpg"
 
 def get_canvas(
         project_name: str
@@ -23,6 +24,7 @@ def save_block_graph(project_name: str, data: SaveCanvasRequest) -> bool:
     block_graph_path =  project_path / block_graph
 
     if create_file(block_graph_path, json_to_str(data.canvas)):
+        save_img_from_base64(project_path, thumbnail, data.thumbnail)
         return True
-
+    
     raise
