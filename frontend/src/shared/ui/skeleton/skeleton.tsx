32863bd7371f683915fde4a66ced1f8a6654928d
allkong <allempti@gmail.com>
2024-11-12 13:18:28 +0900

    feat: Skeleton 컴포넌트 구현

---
+++ frontend/src/shared/ui/skeleton/skeleton.tsx
@@ -0,0 +1,19 @@
+import { Container } from '@shared/ui/skeleton/skeleton.style';
+
+interface SekeletonProps {
+  width?: number;
+  height?: number;
+  count?: number;
+}
+
+const Skeleton = ({ width = 40, height = 10, count = 1 }: SekeletonProps) => {
+  return (
+    <>
+      {Array.from({ length: count }, (_, idx) => idx).map((idx) => (
+        <Container key={idx} width={width} height={height}></Container>
+      ))}
+    </>
+  );
+};
+
+export default Skeleton;
