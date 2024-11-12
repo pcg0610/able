6ec79565749247c0978485aceeea1fad2066b9c5
parkrootseok <parkrootseok@gmail.com>
2024-11-12 13:06:56 +0900

    chore: deploy exception
    - 오타 수정

--- backend/able/src/domain/deploy/exceptions.py
+++ backend/able/src/domain/deploy/exceptions.py
@@ -4,20 +4,20 @@ from src.exceptions import BaseCustomException
 class AlreadyRunException(BaseCustomException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_400_NOT_FOUND,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"이미 실행중입니다."
         )
         
 class AlreadyStopException(BaseCustomException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_400_NOT_FOUND,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"실행중이 아닙니다.."
         )
 
 class AlreadyExistApiException(BaseCustomException):
     def __init__(self):
         super().__init__(
-            status_code=status.HTTP_400_NOT_FOUND,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"이미 존재하는 API입니다."
         )
