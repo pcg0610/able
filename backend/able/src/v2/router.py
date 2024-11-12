3f0e3bd543d66f2c38a35ae6eb57e13a27dde4ba
박상후 <tkdgn407@naver.com>
2024-11-12 11:27:20 +0900

    feat: get_project_api_list (#70)

--- backend/able/src/v2/router.py
+++ backend/able/src/v2/router.py
@@ -1,17 +1,20 @@
-import src.deploy.service as service
+import src.v2.service as service
 from fastapi import APIRouter
-from starlette.responses import Response
-from src.response.utils import accepted, ok, no_content, bad_request
-from src.deploy.schemas import RegisterApiRequest
+
+from src.response.schemas import ResponseModel
+from src.response.utils import ok, no_content, bad_request
+from src.deploy.schemas import ApiInformation
 
 v2_router = router = APIRouter()
 
-@router.get("/api/{project_name}",
-            summary="배포된 API 리스트 조회", description="")
-def get_api_list(project_name: str, page: int, page_size: int):
-    api_list = service.get_api_list(page, page_size)
+@router.get("/api/{project_name}", response_model=ResponseModel[ApiInformation],
+            summary="현재 프로젝트에 배포된 API 리스트 조회", description="")
+def get_project_api_list(project_name: str):
+    api_list = service.get_project_api_list(project_name)
     if api_list is None:
         return bad_request()
     if len(api_list) == 0:
         return no_content()
-    return ok(data=api_list)
+    return ok(
+        data=api_list
+    )
--- backend/able/src/v2/service.py
+++ backend/able/src/v2/service.py
@@ -1,34 +1,28 @@
-from math import ceil
 from typing import List, Optional
 import logging
 
-from src.project.schemas import Project, SelectedProject, UpdatedProject
-from src.file.utils import create_directory, get_directory, delete_directory, create_file, get_file, rename_path, \
-    read_image_file
+from src.deploy.schemas import ApiInformation
+from src.file.constants import METADATA
+from src.file.utils import get_file, get_files
 from src.file.path_manager import PathManager
-from src.utils import str_to_json, json_to_str, encode_image_to_base64
-from src.project.exceptions import ProjectNameAlreadyExistsException
-from src.file.exceptions import FileNotFoundException
-from src.canvas.schemas import Canvas
+from src.utils import str_to_json, encode_image_to_base64
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 path_manager = PathManager()
-METADATA = "metadata.json"
-THUMBNAIL = "thumbnail.jpg"
-BLOCK_GRAPH = "block_graph.json"
 
-def get_project_api_list(title: str) -> Optional[SelectedProject]:
-    metadata_path = path_manager.get_projects_path(title) / METADATA
-    thumbnail_path = path_manager.get_projects_path(title) / THUMBNAIL
+PROJECT_NAME = "project_name"
 
-    data = get_file(metadata_path)
-    project = SelectedProject.model_validate(str_to_json(data))
-    try:
-        project.thumbnail = encode_image_to_base64(read_image_file(thumbnail_path))
-    except FileNotFoundException as e:
-        logger.info(f"썸네일이 존재하지 않음: {project.title}")
-        project.thumbnail = None
+def get_project_api_list(title: str) -> list[ApiInformation]:
+    deploy_path = path_manager.get_deploy_path()
+    api_list = [file_name for file_name in get_files(deploy_path) if file_name != METADATA]
+    api_info_list = []
+    for api in api_list:
+        file_path = deploy_path / api
+        file_data = str_to_json(get_file(file_path))
 
-    return project
+        if file_data[PROJECT_NAME] == title:
+            api_info_list.append(ApiInformation(**file_data))
+
+    return api_info_list
