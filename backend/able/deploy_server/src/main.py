dc259fab6cb05322fc4bd2a6c4be14b122db6525
박찬규 <cham9906@naver.com>
2024-11-13 13:31:51 +0900

    feat: 배포 서버 웹소켓 처리 리팩토링 (#88)

--- backend/able/deploy_server/src/main.py
+++ backend/able/deploy_server/src/main.py
@@ -1,53 +1,66 @@
 import asyncio
+from contextlib import asynccontextmanager
 
 from pathlib import Path
 import uvicorn
 from fastapi import FastAPI, WebSocket
 import argparse
+
+from starlette.websockets import WebSocketDisconnect, WebSocketState
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-app = FastAPI()
+def read_log_file():
+    with open(log_file_path, "r") as file:
+        logs = file.read()
+        return logs
 
-pass
-
-connected_clients = []
-log_file_path = Path(__file__).parent / "server.log"
-loop = asyncio.get_event_loop()
+async def send_updates(message):
+    # 모든 연결된 WebSocket 클라이언트로 메시지 전송
+    for client in connected_clients:
+        if client.client_state == WebSocketState.CONNECTED:
+            await client.send_text(message)
 
 class LogHandler(FileSystemEventHandler):
     def __init__(self):
         super().__init__()
 
-    async def send_updates(self, message):
-        # 모든 연결된 WebSocket 클라이언트로 메시지 전송
-        for client in connected_clients:
-            await client.send_text(message)
-
     def on_modified(self, event):
         # 로그 파일이 변경될 때 이벤트가 발생
         if event.src_path == str(log_file_path):
-            with open(log_file_path, "r") as file:
-                logs = file.read()
-                # asyncio 이벤트 루프에서 비동기 작업을 실행하도록 설정
-                future = asyncio.run_coroutine_threadsafe(self.send_updates(logs), loop)
-                future.result()  # 예외 처리
+            logs = read_log_file()
+            asyncio.run(send_updates(logs))
 
-log_handler = LogHandler()
-observer = Observer()
-observer.schedule(log_handler, path=str(log_file_path.parent), recursive=False)
-observer.start()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    log_handler = LogHandler()
+    observer = Observer()
+    observer.schedule(log_handler, path=str(log_file_path.parent), recursive=False)
+    observer.start()
+
+    yield
+
+connected_clients: list[WebSocket] = []
+log_file_path = Path(__file__).parent / "server.log"
+
+if log_file_path.exists():
+    with open(log_file_path, "w") as f:
+        f.truncate(0)
+
+app = FastAPI(lifespan=lifespan)
+
+pass
+
 
 @app.websocket("/ws/logs")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     connected_clients.append(websocket)
+
     try:
         while True:
-            await asyncio.sleep(1)
-    except Exception as e:
-        print(f"WebSocket error: {e}")
-    finally:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
         connected_clients.remove(websocket)
 
 @app.get("/")
@@ -96,7 +109,4 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    # uvicorn 서버를 asyncio 이벤트 루프에서 실행
-    config = uvicorn.Config(app=app, host="0.0.0.0", port=args.port, log_config=args.log_config)
-    server = uvicorn.Server(config)
-    loop.run_until_complete(server.serve())
+    uvicorn.run(app=app, host="127.0.0.1", port=args.port, log_config=args.log_config)
