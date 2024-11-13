6832d77ab81aa0d492ab2c4f48f42249c0785728
allkong <allempti@gmail.com>
2024-11-13 17:20:55 +0900

    feat: DeployInfo Skeleton 적용

--- frontend/src/features/deploy/ui/common/deploy-info.tsx
+++ frontend/src/features/deploy/ui/common/deploy-info.tsx
@@ -1,14 +1,16 @@
 import * as S from '@features/deploy/ui/common/deploy-into.style';
 
+import { useFetchDeployInfo } from '@features/deploy/api/use-deploy.query';
+
 import RocketIcon from '@icons/rocket.svg?react';
-import { useFetchDeployInfo } from '../../api/use-deploy.query';
+import Skeleton from '@/shared/ui/loading/skeleton';
 
 interface InfoContainerProps {
   title: string;
 }
 
 const InfoContainer = ({ title }: InfoContainerProps) => {
-  const { data: deployInfo } = useFetchDeployInfo();
+  const { data: deployInfo, isFetching } = useFetchDeployInfo();
 
   return (
     <S.InfoWrapper>
@@ -16,19 +18,20 @@ const InfoContainer = ({ title }: InfoContainerProps) => {
         <RocketIcon width={43} height={43} />
         <S.Title>{title}</S.Title>
       </S.TitleSection>
+
       <S.InfoSection>
         <S.InfoText>
           <S.Label>FastAPI</S.Label>
-          <S.Value>{deployInfo?.apiVersion}</S.Value>
+          {isFetching ? <Skeleton width={2.9375} height={1.25} /> : <S.Value>{deployInfo?.apiVersion}</S.Value>}
         </S.InfoText>
         <S.InfoText>
           <S.Label>Port</S.Label>
-          <S.Value>{deployInfo?.port}</S.Value>
+          {isFetching ? <Skeleton width={2.9375} height={1.25} /> : <S.Value>{deployInfo?.port}</S.Value>}
         </S.InfoText>
         <S.InfoText>
           <S.Label>Status</S.Label>
           <S.Value>
-            <S.Status>{deployInfo?.status}</S.Status>
+            {isFetching ? <Skeleton width={2.9375} height={1.25} /> : <S.Status>{deployInfo?.status}</S.Status>}
           </S.Value>
         </S.InfoText>
       </S.InfoSection>
