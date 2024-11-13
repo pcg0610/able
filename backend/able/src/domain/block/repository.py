65c680b171f31be7b4b340cf6eb640e52b76e344
박상후 <tkdgn407@naver.com>
2024-11-13 17:52:49 +0900

    feat: domain/block/router refactoring (#86)

--- backend/able/src/domain/block/repository.py
+++ backend/able/src/domain/block/repository.py
@@ -1,21 +1,46 @@
 from pathlib import Path
+from math import ceil
 
 from src.block.enums import BlockType
+from src.block.exceptions import BlockNotFoundException
 from src.file.path_manager import PathManager
-from src.file.utils import get_directory
-from src.utils import logger
+from src.file.utils import get_directory, get_file, remove_file, create_file
+from src.utils import logger, str_to_json, handle_pagination
+
 
 class BlockRepository:
 
     def __init__(self):
         self.path_manager = PathManager()
+        self.JSON_TEXT = ".json"
+        self.DATA_TEXT = "data"
 
-    def get_target_block_type_directory_paths(self, block_type: BlockType) -> list[Path]:
-        block_type_dir_path = self.path_manager.get_block_path(block_type)
+    # ------ Block Retrieval ------
+    def get_json_block_paths(self, block_type: BlockType) -> list[Path]:
+        block_type_dir_path = self.get_block_type_path(block_type)
         logger.info(f"Searching for blocks of type '{block_type.value}' in {block_type_dir_path}")
 
-        return get_directory(block_type_dir_path)
+        return [path for path in get_directory(block_type_dir_path) if path.suffix == self.JSON_TEXT]
+
+    def get_block_data_by_keyword(self, keyword: str) -> dict:
+        for directory_name in self.get_block_type_directories():
+            if directory_name.name == self.DATA_TEXT:
+                continue
+
+            blocks_path = get_directory(directory_name)
+            for block_path in blocks_path:
+                if block_path.name == f"{keyword}{self.JSON_TEXT}":
+                    return self.get_block_data(block_path)
+
+        raise BlockNotFoundException(keyword)
+
+    # ------ Helper Methods ------
+    def get_block_type_path(self, block_type: BlockType) -> Path:
+        return self.path_manager.get_block_path(block_type)
+
+    def get_block_data(self, block_path: Path) -> dict:
+        data = get_file(block_path)
+        return str_to_json(data)
 
     def get_block_type_directories(self) -> list[Path]:
         return get_directory(self.path_manager.blocks_path)
-
--- backend/able/src/domain/block/service.py
+++ backend/able/src/domain/block/service.py
@@ -1,8 +1,6 @@
 from src.block.enums import BlockType
 from src.block.exceptions import BlockNotFoundException
 from src.block.schemas import Block
-from src.file.utils import get_file, get_directory
-from src.file.path_manager import PathManager
 from src.utils import str_to_json, logger
 from src.domain.block import repository as block_repository
 
@@ -10,9 +8,8 @@ class BlockService:
 
     def __init__(self, repository: block_repository):
         self.repository = repository
-        self.JSON = ".json"
-        self.DATA = "data"
 
+    """
     def find_blocks_by_type(self, block_type: BlockType) -> list[Block]:
         block_paths = self.repository.get_target_block_type_directory_paths(block_type)
 
@@ -28,20 +25,23 @@ class BlockService:
             blocks.append(Block(**str_to_json(data)))
 
         return blocks
+    """
+
+    def find_blocks_by_type(self, block_type: BlockType) -> list[Block]:
+        block_paths = self.repository.get_json_block_paths(block_type)
+
+        blocks = []
+        for block_path in block_paths:
+            logger.debug(f"Loading block from path: {block_path}")
+            block_data = self.repository.get_block_data(block_path)
+            blocks.append(Block(**block_data))
+
+        return blocks
 
     def search(self, keyword: str) -> Block:
-        type_directory_names = self.repository.get_block_type_directories()
-
-        for directory_name in type_directory_names:
-            blocks_path = get_directory(directory_name)
-
-            if directory_name.name == self.DATA:
-                continue
-
-            for block_path in blocks_path:
-                logger.debug(f"Checking path: {block_path}")
-                if block_path.name == f"{keyword}.json":
-                    block_data = get_file(block_path)
-                    return Block(**str_to_json(block_data))
-
-        raise BlockNotFoundException(keyword)
+        try:
+            block_data = self.repository.get_block_data_by_keyword(keyword)
+            return Block(**block_data)
+        except BlockNotFoundException:
+            logger.error(f"Block not found for keyword: {keyword}")
+            raise BlockNotFoundException(keyword)
