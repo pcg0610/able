1d84256cf664b17312b05993f7300e9f11cf8f25
allkong <allempti@gmail.com>
2024-11-14 14:43:45 +0900

    feat: 블록 필드 required 여부에 따라 여닫도록 변경

--- frontend/src/entities/block-node/ui/block-node.tsx
+++ frontend/src/entities/block-node/ui/block-node.tsx
@@ -1,5 +1,5 @@
 import { Position, useReactFlow } from '@xyflow/react';
-import { memo, useMemo } from 'react';
+import { memo, useMemo, useState } from 'react';
 
 import * as S from '@entities/block-node/ui/block-node.style';
 import Common from '@shared/styles/common';
@@ -9,6 +9,7 @@ import type { BlockItem } from '@features/canvas/types/block.type';
 import { capitalizeFirstLetter } from '@shared/utils/formatters.util';
 
 import CustomHandle from '@entities/block-node/ui/custom-handle';
+import ArrowButton from '@/shared/ui/button/arrow-button';
 
 interface BlockNodeProps {
   id: string;
@@ -28,6 +29,7 @@ const BlockNode = ({
   targetPosition = Position.Top,
 }: BlockNodeProps) => {
   const { updateNodeData } = useReactFlow();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const blockColor = useMemo(() => (block?.type ? BLOCK_COLORS[block.type] : Common.colors.gray200), [block.type]);
 
@@ -36,12 +38,22 @@ const BlockNode = ({
     updateNodeData(id, { block: { ...block, fields: updatedFields } });
   };
 
+  const requiredFields = block.fields.filter((field) => field.isRequired);
+  const visibleFields = isOpen ? block.fields : requiredFields;
+
   return (
     <S.Container blockColor={blockColor} isConnected={isConnected} isSelected={isSelected}>
       <CustomHandle type="target" position={targetPosition} connectionCount={CONNECTION_LIMIT_COOUNT} />
-      <S.Label>{capitalizeFirstLetter(block?.name || 'Unknown')}</S.Label>
+      <S.Label>
+        <span>{capitalizeFirstLetter(block?.name || 'Unknown')}</span>
+        <ArrowButton
+          color={Common.colors.white}
+          direction={isOpen ? 'up' : 'down'}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </S.Label>
       <S.FieldWrapper>
-        {block?.fields?.map((field) => (
+        {visibleFields.map((field) => (
           <S.InputWrapper key={field.name} blockColor={blockColor}>
             <S.Name>
               {field.name} {field.isRequired ? '*' : ''}
